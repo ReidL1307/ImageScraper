b0e281146cdaa0473be7cf56d339db131f8870db
ReidL1307 <79946812+ReidL1307@users.noreply.github.com>
2022-09-03 20:24:16 -0600

    Changes made by Reid Leahy

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,37 @@
 const puppeteer = require('puppeteer');
 const express = require("express");
 
+
+const app = express();
+const port = process.env.PORT || 8080;
+
 let links = [];
+const dotEndings = [".gif", ".png", ".jpg"];
 
 async function getImages(link) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    page.on('response', async response => {
-        const url = response.url();
-        if (response.request().resourceType() === 'image') {
-            links.push(url)
-        }
+	links = [];
+	try {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		page.on('response', async response => {
+			const url = response.url();
+			if (response.request().resourceType() === 'image' && dotEndings.some(el => url.endsWith(el))) {
+				links.push(url)
+			}
 
-    });
-    await page.goto(link);
+		});
+		await page.goto(link);
 
-    await browser.close();
+		await browser.close();
+	} catch {
+		links = "INVALID_URL";
+	}
 };
 
-(async () => {
-    await getImages("https://google.com");
+app.get('/', async (req, res) => {
+	await getImages(req.query.site);
+	await res.send(links);
+});
 
-    await console.log(links);
-})();
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
